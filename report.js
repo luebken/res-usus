520c7e8a67e22a84c6748014f1d402e26f73ff88
Matthias Luebken <matthias.luebken@gmail.com>
2020-11-27 15:59:56 +0100

    cleanup for bug report

--- report.js
+++ report.js
@@ -1,12 +1,19 @@
-const http = require('http');
 const request = require('request');
 const moment = require('moment');
 require("moment-duration-format");
 
 const hour = 1000 * 60 * 60
 const day = hour * 24
-const timeframe_windowsize = hour * 12 //TODO test with a full day
+
+// Question 4: Why doesn't this work with 24 hours
+const timeframe_windowsize = hour * 12 
 const timeframe_to = Date.now()
+
+// Question 1: Test with different timeframe_to. 
+// As I was testing I couldn't get different timezones and the 
+// const timeframe_to = Date.parse('2020-11-26 15:00:00')
+
+// Question 2: If I decrease the granularity here I would expect multiple metrics. 
 const granularity = hour * 12
 
 // Check Env & CLI
@@ -68,7 +75,7 @@ function buildMetrics(specs) {
 
 // --- Request ---
 
-function requestReport(groupBy, metrics, callback) {
+function requestReport(groupBy, metrics) {
 
     const options = buildRequestOptions(groupBy, metrics)
 
@@ -76,6 +83,7 @@ function requestReport(groupBy, metrics, callback) {
         if (error) console.error('error:', error);
         if (response.statusCode != 200) {
             console.debug('SatusCode:', response && response.statusCode);
+            console.debug('Body:', body);
             return
         }
         var body = JSON.parse(body)
@@ -84,8 +92,7 @@ function requestReport(groupBy, metrics, callback) {
             return
         }
 
-        var report = buildReport(body.data, groupBy)
-        callback(report)
+        printReport(body.data, groupBy)
     });
 }
 
@@ -93,25 +100,12 @@ function requestReport(groupBy, metrics, callback) {
 // --- Server ---
 
 
-console.log("Starting server on 8080")
-http.createServer(function (req, res) {
-    console.log("Received Request")
+console.log("Received Request")
 
-    var dockerCpuMetrics = { type: "docker", granularity: granularity, metric: "cpu.total_usage", aggregations: ["MEAN", "P95", "P99", "MAX"] }
-    var dockerMemoryMetrics = { type: "docker", granularity: granularity, metric: "memory.usage", aggregations: ["MEAN", "P95", "P99", "MAX"] }
+var dockerCpuMetrics = { type: "docker", granularity: granularity, metric: "cpu.total_usage", aggregations: ["MEAN"] }
+var groupByNamespace = { groupByKey: "kubernetes.namespace.name", groupByLabel: "Namespace" }
 
-    var groupByNamespace = { groupByKey: "kubernetes.namespace.name", groupByLabel: "Namespace" }
-    var groupByLabel = { groupByKey: "kubernetes.pod.label", groupByLabel: "Pod Label" }
-
-    requestReport(groupByNamespace, [dockerCpuMetrics, dockerMemoryMetrics], function (report1) {
-        requestReport(groupByLabel, [dockerCpuMetrics, dockerMemoryMetrics], function (report2) {
-            res.write(report1)
-            res.write(report2)
-            res.end()
-        }
-        )
-    })
-}).listen(8080);
+requestReport(groupByNamespace, [dockerCpuMetrics])
 
 
 
@@ -119,36 +113,24 @@ http.createServer(function (req, res) {
 
 // --- Build Report ---
 
-function buildReport(data, groupBy) {
-    var report = "";
-    report += 'Usage Report for Kubernetes cluster "' + k8s_cluster + '" with ' + data.items.length + ' ' + groupBy.groupByLabel + "s. "
-    report += "From: " + moment(timeframe_to - timeframe_windowsize).format('DD MMMM YYYY HH:mm:ss');
-    report += " to: " + moment(timeframe_to).format('DD MMMM YYYY HH:mm:ss') + "\n";
-    report += "Type,Group,Container,CPU Usage (Mean),CPU Usage (P95),CPU Usage (P99),CPU Usage (Max),Memory Usage (Mean),Memory Usage (P95),Memory Usage (P99),Memory Usage (Max)\n"
+function printReport(data, groupBy) {
+    console.log("\n")
+    console.log('Usage Report for Kubernetes cluster "' + k8s_cluster + '" with ' + data.items.length + ' ' + groupBy.groupByLabel + "s. ")
+    console.log("From " + moment(timeframe_to - timeframe_windowsize).toString());
+    console.log("To " + moment(timeframe_to).toString());
+    console.log("\n")
+
     data.items.forEach(item => {
-        report += groupBy.groupByLabel + "," + item.tags[groupBy.groupByKey] + "," + item.count + ","
-        report += metricStringPercent(item.metrics, "cpu.total_usage.MEAN." + timeframe_windowsize) + ","
-        report += metricStringPercent(item.metrics, "cpu.total_usage.P95." + timeframe_windowsize) + ","
-        report += metricStringPercent(item.metrics, "cpu.total_usage.P99." + timeframe_windowsize) + ","
-        report += metricStringPercent(item.metrics, "cpu.total_usage.MAX." + timeframe_windowsize) + ","
-        report += metricStringByte(item.metrics, "memory.usage.MEAN." + timeframe_windowsize) + ","
-        report += metricStringByte(item.metrics, "memory.usage.P95." + timeframe_windowsize) + ","
-        report += metricStringByte(item.metrics, "memory.usage.P99." + timeframe_windowsize) + ","
-        report += metricStringByte(item.metrics, "memory.usage.MAX." + timeframe_windowsize)
-        report += "\n"
+        console.log(item.tags)
+        console.log(item.metrics)
+        if (item.metrics['cpu.total_usage.MEAN.43200000'])
+            // Question 3: What is this metric timestamp?
+            // Why is this different from timeframe_to?
+            console.log("Metric timestamp: " + moment(item.metrics['cpu.total_usage.MEAN.43200000'][0][0]).toString())
+        console.log("\n")
     });
-    return report;
 }
 
-function metricStringPercent(metrics, metricKey, label) {
-    if (metrics[metricKey]) { // TODO for each metric not just [0][1]
-        return (metrics[metricKey][0][1] * 100).toFixed(2) + "%";
-    }
-}
 
-function metricStringByte(metrics, metricKey, label) {
-    if (metrics[metricKey]) { // TODO for each metric not just [0][1]
-        return (metrics[metricKey][0][1] / 1024 / 1024).toFixed(2) + "MB";
-    }
-}
+
 
